13b59daf8ac1130c6706366150c56f75f5e8c0b0
wjvelez <wjvelez@espol.edu.ec>
2017-02-07 14:26:17 -0500

    pruebas con la interfaz

--- prueba-qt.py
+++ prueba-qt.py
@@ -1,6 +1,13 @@
+
 import sys
+import numpy as np
+import cv2
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
 app = QApplication(sys.argv)
 button = QPushButton("Hello World", None)
 button.show()
 app.exec_()
+
+
