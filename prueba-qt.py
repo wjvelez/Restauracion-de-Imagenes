ff4c2d70609d4fc2c0aa040aa2385f497d72acd1
wjvelez <wjvelez@espol.edu.ec>
2017-02-07 14:56:26 -0500

    ventana principal

--- prueba-qt.py
+++ prueba-qt.py
@@ -2,12 +2,17 @@
 import sys
 import numpy as np
 import cv2
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt4 import QtGui
 
-app = QApplication(sys.argv)
-button = QPushButton("Hello World", None)
-button.show()
-app.exec_()
+class Window(QtGui.QMainWindow):
 
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("Restaurador de Imagenes")
+        self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.show()
 
+app = QtGui.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
